9d06a949e10624a4cb528939bcf0606452eaf041
Nicolas <nicoceriani@gmail.com>
2020-03-03 16:27:06 +0100

    WIP Level tool

---
+++ Assets/Scripts/Level/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelElementRoomSettings
+{
+    public LevelElement levelElement;
+    public Vector3 pos;
+}
+
+[CreateAssetMenu(fileName = "New Room.asset", menuName = "Level/Room", order = 140)]
+public class Room : ScriptableObject
+{
+    public List<LevelElementRoomSettings> elements = new List<LevelElementRoomSettings>();
+
+    public int selectedBrush = 0;
+}
