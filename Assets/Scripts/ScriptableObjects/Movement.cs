d5577a6c28d4f0b264d7e2e66dd04bed704ea8bc
Nicolas <nicoceriani@gmail.com>
2020-03-03 10:07:20 +0100

    Scriptable objects

--- Assets/Scripts/ScriptableObjects/Movement.cs
+++ Assets/Scripts/ScriptableObjects/Movement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Movement", menuName = "Gameplay/Movement", order = 150)]
 public class Movement : ScriptableObject
 {
-    
+    public Range range;
 }
--- Assets/Scripts/ScriptableObjects/Stats.cs
+++ Assets/Scripts/ScriptableObjects/Stats.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Unit Stats", menuName = "Gameplay/UnitStats", order = 150)]
 public class Stats : ScriptableObject
 {
-
+    public float health = 1f;
 }
