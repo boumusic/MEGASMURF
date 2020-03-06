8dfc012534b0bdd06b37706f048cded4a30b4661
Nicolas <nicoceriani@gmail.com>
2020-03-06 18:03:53 +0100

    Integration Mouths & Enemy

--- Assets/Scripts/Visuals/FloatingAnimation.cs
+++ Assets/Scripts/Visuals/FloatingAnimation.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class FloatingAnimation : MonoBehaviour
 {
-    [SerializeField] private float amplitude = 1f;
-    [SerializeField] private float speed = 1f;
-
-    private Vector3 initPos;
-
-    private void Start()
-    {
-        initPos = transform.localPosition;
-    }
-
+    [SerializeField] private float amplitude = 0.005f;
+    [SerializeField] private float speed = 0.01f;
+    [SerializeField, Range(-1,1)] private float offset = 0f;
+    
     private void Update()
     {
-        float sin = Mathf.Sin(Time.frameCount * speed + (initPos.x+initPos.y + initPos.z)) * amplitude;
+        float sin = Mathf.Sin(Time.frameCount * speed + offset * Mathf.PI) * amplitude;
         transform.localPosition = new Vector3(transform.localPosition.x, sin, transform.localPosition.z);
     }
 }
