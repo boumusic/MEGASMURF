d75aaacaeab9f106fa3be8de89cfb188ff12d856
PIAU Timothée <t.piau@rubika-edu.com>
2020-03-10 16:36:08 +0100

    Maestro UnitBase

--- Assets/Scripts/Unit/UnitPage.cs
+++ Assets/Scripts/Unit/UnitPage.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct UnitPage
+public struct UnitBlueprint
 {
-    public BaseUnitType unitType;
     public GameObject unitPrefab;
 }
