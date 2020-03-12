e23c5b73f743431dc758004ec69d4a4abf8123c0
Nicolas <nicoceriani@gmail.com>
2020-03-12 23:47:02 +0100

    Fix Wine

---
+++ Assets/Scripts/Visuals/RandomAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public int min;
+    public int max;
+
+    public void Start()
+    {
+        float blend = Random.Range(min, max);
+        animator.SetFloat("Blend", blend);
+    }
+}
