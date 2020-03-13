9642565df3270af8bc38439cd471eb285a98d7c9
Nicolas <nicoceriani@gmail.com>
2020-03-13 12:35:18 +0100

    POOL MANAGER INSTANTIATE ON AWAKE

---
+++ Assets/ShapeSelectionButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSelectionButton : UIButton
+{
+    public BaseUnitType type;
+
+    public override void OnClick()
+    {
+        base.OnClick();
+        switch (type)
+        {
+            case BaseUnitType.Triangle:
+                InputManager.instance.OnTriangleButtonPress();
+                break;
+            case BaseUnitType.Circle:
+                InputManager.instance.OnCircleButtonPress();
+                break;
+            case BaseUnitType.Square:
+                InputManager.instance.OnSquareButtonPress();
+                break;
+        }
+    }
+
+}
