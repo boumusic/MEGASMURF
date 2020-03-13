70c35cff2fa4d531f8b91e7c3a18bf3ef5d40e72
Nicolas <nicoceriani@gmail.com>
2020-03-13 17:10:11 +0100

    commit

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
