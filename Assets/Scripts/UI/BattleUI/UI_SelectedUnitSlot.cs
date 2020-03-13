c02a84ae4b897f9fa1477d51726b03e31049a6f6
Djuloh <j.tien@rubika-edu.com>
2020-03-13 14:20:53 +0100

    Selected Shape Slot : les anims se jouent  quand on launch les fonctions ! reste à les launch !

--- Assets/Scripts/UI/BattleUI/UI_SelectedUnitSlot.cs
+++ Assets/Scripts/UI/BattleUI/UI_SelectedUnitSlot.cs
@@ -26,7 +26,8 @@ public class UI_SelectedUnitSlot : UIElement
 
     private bool isShapeUnit;
     private int unitMergeLevel;
-    
+
+    private Animator anim;
 
     public Unit SelectedUnit { get; private set; }
 
@@ -36,6 +37,7 @@ public class UI_SelectedUnitSlot : UIElement
     {
         actionButtonSpriteState = new SpriteState();
         unitMergeLevel = 0;
+        anim = gameObject.GetComponent<Animator>();
     }
 
     public void SelectUnit(Unit unit)
@@ -179,15 +181,20 @@ public class UI_SelectedUnitSlot : UIElement
     }
 
     //ANIMATIONS
-
+    private void Update()
+    {
+       
+    }
     public void SelectedDamageAnim()
     {
         //Quand le joueur prend des dégâts
+        anim.Play("SelectedDamage");
     }
 
     public void SelectedSwitchAnim()
     {
         //Quand le joueur change d'unités
+        anim.Play("SelectedSwitch");
     }
 
     public void SelectedBattleModeOnAnim()
@@ -199,6 +206,27 @@ public class UI_SelectedUnitSlot : UIElement
     {
         //Mode attaque desactivé
     }
+
+    public void SelectedSoloToDuo()
+    {
+        anim.Play("SelectedSoloToDuo");
+
+    }
+    public void SelectedDuoToTrio()
+    {
+        anim.Play("SelectedDuoToTrio");
+    }
+
+    public void SelectedInvokeShapesOn()
+    {
+        anim.Play("SelectedInvokeShapesOn");
+    }
+
+    public void SelectedInvokeShapesOff()
+    {
+        anim.Play("SelectedInvokeShapesOff");
+    }
+
 
     public void UpdateName(Unit unit)
     {
